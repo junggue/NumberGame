32a1f80c32621b09092b18dd0b3df40e6ecf6898
laurenritter <laurenritter@client-104-39-66-203.mobility.psu.edu>
2016-02-22 22:04:33 -0500

    Worked on Apple class

--- NumberGame/src/numbergame/Apple.java
+++ NumberGame/src/numbergame/Apple.java
@@ -5,10 +5,28 @@
  */
 package numbergame;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import javafx.scene.shape.Circle;
+
 /**
  *
  * @author laurenritter
  */
-public class Apple extends GameController{
+public class Apple extends Circle{
+    private Circle apple = null;
+    private final double radius;
+    private final Color appleColor;
     
+    Apple(){
+        apple = new Circle();
+        radius = 50.0;
+        appleColor = Color.red;
+    }
+    
+    public void draw(Graphics graph){
+        graph.setColor(appleColor);
+        //graph.(0, 0, this.apple, this.apple);
+    }
 }
