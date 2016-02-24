e44568d3c813e48346dd967449aa082856035338
laurenritter <laurenritter@client-104-39-43-39.mobility.psu.edu>
2016-02-24 15:18:21 -0500

    Instructions class

---
+++ NumberGame/src/numbergame/Instructions.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numbergame;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author laurenritter
+ */
+public class Instructions extends JPanel{
+    private JButton exitButton;
+    private JButton backButton;
+    private JButton startButton;
+    GameController theGameController;
+    
+    public Instructions(){
+        exitButton = new JButton("Exit");
+        backButton = new JButton("Back to Main Screen");
+        startButton = new JButton("Start game");
+        System.out.println("On the top of the screen you will see a number along with 'apples' containing numbers. Click the apples with the correct numbers to reach the goal number.");
+    }
+}
