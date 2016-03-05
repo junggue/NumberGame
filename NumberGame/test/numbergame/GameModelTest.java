fd9b965092c22a7a60821f2e2f5378e92f5ae7dc
junggue <junggue.yang@gmail.com>
2016-03-05 05:09:37 -0500

    added meaningful unit tests for random number

--- NumberGame/test/numbergame/GameModelTest.java
+++ NumberGame/test/numbergame/GameModelTest.java
@@ -17,108 +17,161 @@ import static org.junit.Assert.*;
  * @author laurenritter
  */
 public class GameModelTest {
-    
+
     public GameModelTest() {
     }
 
-    /**
-     * Test of statusResult method, of class GameModel.
-     */
+//    /**
+//     * Test of errorMessage method, of class GameModel.
+//     */
+//    @Test
+//    public void testErrorMessage() {
+//        System.out.println("errorMessage");
+//        GameModel instance = new GameModel();
+//        String expResult = "That number is already chosen";
+//        String result = instance.errorMessage();
+//        assertEquals(expResult, result);
+//    }
+    
     @Test
-    public void testStatusResultWhenPlayerWins() {
-        System.out.println("statusResult");
-        GameModel instance = new GameModel();
-        String expResult = "You Won!";
-        String result = instance.statusResult();
-        assertEquals(expResult, result);
+    public void testGetRandomNumisRandom1() {
+        System.out.println("getRandomNum");
+        int start = 1;
+        int end = 1;
+        int randomNum;
+        int result;
+
+        for (int i = 0; i < 1000000; i++) {
+
+            randomNum = GameModel.getRandomNum(start, end);
+            
+            if(start <= randomNum && randomNum <=end){
+               
+            }else{
+                fail("failed");
+            }
+        }
+    }
+    @Test
+    public void testGetRandomNumisRandom2() {
+        System.out.println("getRandomNum");
+        int start = 2;
+        int end = 2;
+        int randomNum;
+        int result;
+
+        for (int i = 0; i < 1000000; i++) {
+
+            randomNum = GameModel.getRandomNum(start, end);
+            
+            if(start <= randomNum && randomNum <=end){
+               
+            }else{
+                fail("failed");
+            }
+        }
     }
 
-    /**
-     * Test of statusRestart method, of class GameModel.
-     */
+
     @Test
-    public void testStatusRestartWhenYouLost() {
-        System.out.println("statusRestart");
-        GameModel instance = new GameModel();
-        String expResult = "You lost";
-        String result = instance.statusRestart();
-        assertEquals(expResult, result);
+    public void testGetRandomNumisRandom0() {
+        System.out.println("getRandomNum");
+        int start = 0;
+        int end = 0;
+        int randomNum;
+        int result;
+
+        for (int i = 0; i < 1000000; i++) {
+
+            randomNum = GameModel.getRandomNum(start, end);
+            
+            if(start <= randomNum && randomNum <=end){
+               
+            }else{
+                fail("failed");
+            }
+        }
     }
 
-    /**
-     * Test of errorMessage method, of class GameModel.
-     */
     @Test
-    public void testErrorMessage() {
-        System.out.println("errorMessage");
-        GameModel instance = new GameModel();
-        String expResult = "That number is already chosen";
-        String result = instance.errorMessage();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of getRandomNum method, of class GameModel.
-     */
-    @Test
-    public void testGetRandomNumWhenNumIs9() {
+    public void testGetRandomNumisRandom1to5() {
         System.out.println("getRandomNum");
-        int start = 0;
-        int end = 9;
-        int expResult = 9;
-        int result = GameModel.getRandomNum(start, end);
-        assertEquals(expResult, result);
-    }
-    
-    /**
-     * Test of getRandomNum method, of class GameModel.
-     */
-    @Test
-    public void testGetRandomNumWhenNumIs3() {
-        System.out.println("getRandomNum");
-        int start = 0;
-        int end = 3;
-        int expResult = 3;
-        int result = GameModel.getRandomNum(start, end);
-        assertEquals(expResult, result);
-    }
-    
-    /**
-     * Test of getRandomNum method, of class GameModel.
-     */
-    @Test
-    public void testGetRandomNumis4() {
-        System.out.println("getRandomNum");
-        int start = 0;
-        int end = 4;
-        int expResult = 4;
-        int result = GameModel.getRandomNum(start, end);
-        assertEquals(expResult, result);
-    }
-    
-    /**
-     * Test of getRandomNum method, of class GameModel.
-     */
-    @Test
-    public void testGetRandomNumis5() {
-        System.out.println("getRandomNum");
-        int start = 0;
+        int start = 1;
         int end = 5;
-        int expResult = 5;
-        int result = GameModel.getRandomNum(start, end);
-        assertEquals(expResult, result);
+        int randomNum;
+        int result;
+
+        for (int i = 0; i < 1000000; i++) {
+
+            randomNum = GameModel.getRandomNum(start, end);
+            
+            if(start <= randomNum && randomNum <=end){
+               
+            }else{
+                fail("failed");
+            }
+        }
     }
     
-    /**
-     * Test of getRandomNum method, of class GameModel.
-     */
     @Test
-    public void testGetRandomNumIs10() {
+    public void testGetRandomNumisRandom1to4() {
         System.out.println("getRandomNum");
-        int start = 0;
-        int end = 10;
-        int expResult = 10;
-        int result = GameModel.getRandomNum(start, end);
-        assertEquals(expResult, result);
+        int start = 1;
+        int end = 4;
+        int randomNum;
+        int result;
+
+        for (int i = 0; i < 1000000; i++) {
+
+            randomNum = GameModel.getRandomNum(start, end);
+            
+            if(start <= randomNum && randomNum <=end){
+               
+            }else{
+                fail("failed");
+            }
+        }
     }
+    
+    @Test
+    public void testGetRandomNumisRandom1to3() {
+        System.out.println("getRandomNum");
+        int start = 1;
+        int end = 3;
+        int randomNum;
+        int result;
+
+        for (int i = 0; i < 1000000; i++) {
+
+            randomNum = GameModel.getRandomNum(start, end);
+            
+            if(start <= randomNum && randomNum <=end){
+               
+            }else{
+                fail("failed");
+            }
+        }
+    }
+    
+    @Test
+    public void testGetRandomNumisRandom1to2() {
+        System.out.println("getRandomNum");
+        int start = 1;
+        int end = 2;
+        int randomNum;
+        int result;
+
+        for (int i = 0; i < 1000000; i++) {
+
+            randomNum = GameModel.getRandomNum(start, end);
+            
+            if(start <= randomNum && randomNum <=end){
+               
+            }else{
+                fail("failed");
+            }
+        }
+    }
+
+
 }
